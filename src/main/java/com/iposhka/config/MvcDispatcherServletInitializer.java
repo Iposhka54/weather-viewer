2329085e0d5dc6f3f0b7e90baacbdd39c3dfc2ea
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-02-02 16:16:42 +0300

    Add: Add DispatcherServlet initializer

---
+++ src/main/java/com/iposhka/config/MvcDispatcherServletInitializer.java
@@ -0,0 +1,31 @@
+package com.iposhka.config;
+
+import jakarta.servlet.Filter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?> @Nullable [] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?> @Nullable [] getServletConfigClasses() {
+        return new Class[]{ApplicationConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return new Filter[] { encodingFilter };
+    }
+}
