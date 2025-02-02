659c04d0e956917d19a4052936929055da719b0c
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-02-02 16:54:24 +0300

    Add: Add user entity

---
+++ src/main/java/com/iposhka/model/User.java
@@ -0,0 +1,24 @@
+package com.iposhka.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "login", unique = true, nullable = false)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
