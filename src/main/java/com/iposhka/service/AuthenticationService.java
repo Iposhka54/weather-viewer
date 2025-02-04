3199dfcb85f6412f20889608b7c385f3310ea870
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-02-04 21:06:27 +0300

    Feat: add crypt password in service

--- src/main/java/com/iposhka/service/AuthenticationService.java
+++ src/main/java/com/iposhka/service/AuthenticationService.java
@@ -1,26 +1,27 @@
 package com.iposhka.service;
 
 import com.iposhka.dto.CreateUserDto;
+import com.iposhka.mapper.UserMapper;
 import com.iposhka.model.User;
 import com.iposhka.repository.UserRepository;
+import com.iposhka.util.CryptUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public AuthenticationService(UserRepository userRepository){
+    public AuthenticationService(UserRepository userRepository, UserMapper userMapper){
 
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     public void signIn(CreateUserDto userDto){
-        //need mapper and password service with encrypt method
-        User user = User.builder()
-                .login(userDto.getUsername())
-                .password(userDto.getPassword())
-                .build();
-
-        userRepository.save(user);
+        User entity = userMapper.toEntity(userDto);
+        String cryptPassword = CryptUtil.crypt(entity.getPassword());
+        entity.setPassword(cryptPassword);
+        userRepository.save(entity);
     }
 }
