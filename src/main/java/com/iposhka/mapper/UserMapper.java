bcd14a80eb522d4c26aa4a5fce2d3043c00cc69a
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-02-04 21:09:29 +0300

    Fix: add mapping condition

--- src/main/java/com/iposhka/mapper/UserMapper.java
+++ src/main/java/com/iposhka/mapper/UserMapper.java
@@ -4,13 +4,16 @@ import com.iposhka.dto.CreateUserDto;
 import com.iposhka.dto.UserDto;
 import com.iposhka.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(source = "username", target = "login")
     User toEntity(CreateUserDto dto);
 
+    @Mapping(source = "username", target = "login")
     User toEntity(UserDto dto);
 }
