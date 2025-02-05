06ff30994da26464c5af9700c2f6eb01d7077fac
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-02-05 17:15:49 +0300

    Refactor: merge two controllers in one

--- src/main/java/com/iposhka/controller/AuthController.java
+++ src/main/java/com/iposhka/controller/AuthController.java
@@ -1,11 +1,11 @@
 package com.iposhka.controller;
 
+import com.iposhka.dto.CreateUserDto;
 import com.iposhka.dto.SessionDto;
 import com.iposhka.dto.UserLoginDto;
 import com.iposhka.service.AuthenticationService;
 import com.iposhka.service.SessionService;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
@@ -14,22 +14,42 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class SignInController {
+@RequestMapping("/auth")
+public class AuthController {
     private final AuthenticationService authService;
 
-    public SignInController(AuthenticationService authService) {
+    public AuthController(AuthenticationService authService) {
         this.authService = authService;
     }
 
+    @GetMapping("/sign-up")
+    public String signUp(@ModelAttribute("createUser") CreateUserDto userDto) {
+        return "sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUp(@ModelAttribute("createUser")
+                         @Valid CreateUserDto userDto,
+                         BindingResult bindingResult,
+                         Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("bindingResult", bindingResult);
+            return "/sign-up";
+        }
+        authService.signUp(userDto);
+        return "redirect:/sign-in";
+    }
+
     @GetMapping("/sign-in")
-    public String signIn(@ModelAttribute("userDto") UserLoginDto user) {
+    public String signIn(@ModelAttribute("userLoginDto") UserLoginDto user) {
         return "sign-in";
     }
 
     @PostMapping("/sign-in")
-    public String signIn(@ModelAttribute("userDto")
+    public String signIn(@ModelAttribute("userLoginDto")
                          @Valid UserLoginDto userLoginDto,
                          BindingResult bindingResult,
                          Model model,
@@ -50,6 +70,6 @@ public class SignInController {
 
         res.addCookie(cookie);
 
-        return "/home";
+        return "redirect:/home";
     }
 }
